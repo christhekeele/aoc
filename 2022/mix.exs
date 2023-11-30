4882661c5ef75809bdee4fbd79c72b5da68ae145
Chris Keele <christhekeele@gmail.com>
2023-11-30 14:46:38 -0600

    Create seperate projects for each year.

--- 2022/mix.exs
+++ 2022/mix.exs
@@ -6,10 +6,6 @@ defmodule AoC.TwentyTwentyTwo.MixProject do
       app: :"2022",
       elixirc_paths: ["day"],
       version: "0.1.0",
-      build_path: "../../_build",
-      config_path: "../../config/config.exs",
-      deps_path: "../../deps",
-      lockfile: "../../mix.lock",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
